a6ac445f4b3c9cde8c6c41774d32c0ee208e588f
saucyy <131738356+Berrytree5@users.noreply.github.com>
2023-10-10 18:28:55 -0700

    Cleaned script for better understanding

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,71 @@
-// Assignment Code
+// Get a reference to the "Generate" button element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function generatePassword() {
-const specialCharacters = [" !#$%&'()*+,-./:;<=>?@[^_`{|}~"];
+// Define character sets
+const specialCharacters = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-const numbers = ["0123456789"];
-  
-//Var inputs
-  var input = prompt("Choose a length of at least 8 characters and no more than 44 characters:")
-  var inputnumbers = prompt("Does your code contain Numbers? (ok or cancel):");
-  var inputlowerCase = prompt("Does you code use lowercase letters? (ok or cancel):");
-  var inputupperCase = prompt("Does your code use uppercase letters? (ok or cancel):");
-  var inputspecialCharacters = prompt("Does you code contain special characters? (ok or cancel):");
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
 
-  var characters = [];
-//if else statements
-  if (input >= 8 && input <= 44) {
-    if (inputnumbers === "ok") {
-    characters.push(...numbers);
-    }
-    if (inputlowerCase === "ok") {
-    characters.push(...lowerCase);
-    }
-    if (inputupperCase === "ok") {
-    characters.push(...upperCase);
-    }
-    if (inputspecialCharacters === "ok") {
-    characters.push(...specialCharacters);
-    }
-  } else {
-  alert("The length of the password must be between 8 and 44 characters.");
-  return;
+// Function to generate a random password
+function generatePassword() {
+  // Get user input for password length
+  var length = prompt("Choose a password length (between 8 and 44 characters):");
+
+  // Check if the user canceled the prompt or entered an invalid length
+  if (length === null) {
+    return; // User canceled
   }
+
+  length = parseInt(length); // Convert to a number
+
+  if (isNaN(length) || length < 8 || length > 44) {
+    alert("Invalid password length. Please enter a number between 8 and 44.");
+    return;
+  }
+
+  // Get user preferences for including character types
+  var includeNumbers = confirm("Include numbers?");
+  var includeLowerCase = confirm("Include lowercase letters?");
+  var includeUpperCase = confirm("Include uppercase letters?");
+  var includeSpecialCharacters = confirm("Include special characters?");
+
+  if (!includeNumbers && !includeLowerCase && !includeUpperCase && !includeSpecialCharacters) {
+    alert("You must include at least one character type.");
+    return;
+  }
+
+  // Build the character set based on user preferences
+  var charSet = "";
+
+  if (includeNumbers) {
+    charSet += numbers;
+  }
+  if (includeLowerCase) {
+    charSet += lowerCase;
+  }
+  if (includeUpperCase) {
+    charSet += upperCase;
+  }
+  if (includeSpecialCharacters) {
+    charSet += specialCharacters;
+  }
+
+  // Generate the password
   var password = "";
-  for (var i = 0; i < input; i++) {
-      password += characters[Math.floor(Math.random() * characters.length)];
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charSet.length);
+    password += charSet.charAt(randomIndex);
   }
-    return password;
+
+  return password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", function(){
-var password = generatePassword();
-
-var passwordText = document.querySelector("#password");
-passwordText.value = password;
+// Add an event listener to the "Generate" button
+generateBtn.addEventListener("click", function() {
+  var password = generatePassword();
+  if (password) {
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 });
